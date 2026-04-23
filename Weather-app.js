3fe0069b876d47cf14eccb0b27f85930d06c2fc5
MJamrozik-ux <mikolajjamrozik81@gmail.com>
2026-04-23 13:20:56 +0200

    wyswietlanie danych i wybor miasta

--- Weather-app.js
+++ Weather-app.js
@@ -1,5 +1,5 @@
 const API_KEY = "fb1eda3a318bd46aa9091ec92ba73e5c";
-let city = "Krakow";
+let city = localStorage.getItem("city");
 
 function getDayName(dateString) {
     let date = new Date(dateString);
@@ -95,19 +95,30 @@ async function getForecast() {
 
     if (!response.ok) {
         console.log("API error: " + response.status);
+        window.location.href = "Weather-app0.html";
+        window.alert("Invalid city")
         return;
     }
 
     let data = await response.json();
 
     let forecast = formatForecastData(data);
-    console.log("3 days:", forecast);
-
-    if (forecast.length > 0) {
-        let exampleDate = forecast[0].date;
-        let singleDay = getWeatherForDate(data, "2026-04-26");
-        console.log("Targetdate:", singleDay);
+    for (let i = 1; i < forecast.length; i++) {
+        let day = forecast[i];
+        let dayNumber = i + 1;
+        document.getElementById(`d${dayNumber}n`).textContent = `${day.day}`
+        document.getElementById(`d${dayNumber}img`).src = "https://openweathermap.org/img/wn/" + day.icon + ".png";
+        
     }
+    let details = forecast[0]
+    document.getElementById("d1n").textContent = `${details.day}  ${details.date}` 
+    document.getElementById("h2").textContent = `${details.temp}°`
+    let text = details.description;
+    let formattedText = text.charAt(0).toUpperCase() + text.slice(1);
+    document.getElementById("h3").textContent = formattedText;
+    document.getElementById(`d1img`).src = "https://openweathermap.org/img/wn/" + details.icon + ".png";
+
+
 }
 
 getForecast();
